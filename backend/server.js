110503a1d768f047c6d8826ac776530a77eab74b
ardhisasongko <ardhisasongko69@guru.smp.belajar.id>
2026-02-13 04:50:21 +0700

    fix: update CORS configuration to specify allowed origins

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,11 @@ const PORT = process.env.PORT || 5000;
 console.log('CORS allowed origins:', process.env.FRONTEND_URL);
 app.use(
   cors({
-    origin: '*',
+    origin: [
+      process.env.FRONTEND_URL,
+      'http://localhost:5000',
+      'https://tka-ardhi.netlify.app',
+    ],
     credentials: true,
   }),
 );
